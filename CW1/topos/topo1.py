6340efb498023b25fd2e03f670bb9e3502788999
keeganleeenxu <kxl20@ic.ac.uk>
2024-10-29 22:28:34 +0000

    added topo1

--- CW1/topos/topo1.py
+++ CW1/topos/topo1.py
@@ -8,10 +8,15 @@ from mininet.cli import CLI
 
 class part1_topo(Topo):
     def build(self):
-        pass
-        # switch1 = self.addSwitch('switchname')
-        # host1 = self.addHost('hostname')
-        # self.addLink(hostname,switchname)
+        switch1 = self.addSwitch('switch1')
+        host1 = self.addHost('host1', mac='00:00:00:00:00:01', ip='192.168.0.1/24')
+        host2 = self.addHost('host2', mac='00:00:00:00:00:02', ip='192.168.0.2/24')
+        host3 = self.addHost('host3', mac='00:00:00:00:00:03', ip='192.168.0.3/24')
+        host4 = self.addHost('host4', mac='00:00:00:00:00:04', ip='192.168.0.4/24')
+        self.addLink(host1, switch1)
+        self.addLink(host2, switch1)
+        self.addLink(host3, switch1)
+        self.addLink(host4, switch1)
 
 
 topos = {"part1": part1_topo}
